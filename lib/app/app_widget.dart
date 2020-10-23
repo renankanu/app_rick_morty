d7ce0b75af8e7487de67e045df6e07cbd045cec4
renankanu <renankanu@gmail.com>
2020-10-22 22:24:42 -0300

    Feature - remove banner

--- lib/app/app_widget.dart
+++ lib/app/app_widget.dart
@@ -9,7 +9,7 @@ class AppWidget extends StatelessWidget {
       navigatorKey: Modular.navigatorKey,
       title: 'Rick e Morty',
       theme: ThemeData(
-        primarySwatch: Colors.blue,
+        primarySwatch: Colors.red,
       ),
       initialRoute: '/',
       onGenerateRoute: Modular.generateRoute,
